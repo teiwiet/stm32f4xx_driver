42e551318f9fbe2948fe50838a3912b0469cce3a
teiwiet <omegalmao1.3@gmail.com>
2025-06-24 09:46:35 +0700

    first commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ D:/Project/stm32/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h
+D:/Project/stm32/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h:
---
+++ drivers/Inc/stm32f411xx.h
@@ -0,0 +1,15 @@
+/*
+ * stm32f411xx.h
+ *
+ *  Created on: Mar 11, 2025
+ *      Author: omega
+ */
+#ifndef INC_STM32F411XX_H_
+#define INC_STM32F411XX_H_
+// base address
+#define FLASH_BASEADDR          0x08000000U
+#define SRAM1_BASEADDR          0x20000000U
+#define SRAM2_BASEADDR          
+#define SRAM                    SRAM1_BASEADDR     
+
+#endif /* INC_STM32F411XX_H_ */
