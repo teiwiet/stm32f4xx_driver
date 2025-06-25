cdc8a19e9259d2d1f71ca0a165ab558de164e8f9
teiwiet <omegalmao1.3@gmail.com>
2025-06-25 09:40:52 +0700

    update

--- Src/main.c
+++ Src/main.c
@@ -16,14 +16,15 @@
  ******************************************************************************
  */
 
+#include "../drivers/Inc/stm32f411xx.h"
 #include <stdint.h>
-#include "stm32f411xx.h"
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning                                                                       \
+    "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-int main(void)
-{
-    /* Loop forever */
-	for(;;);
+int main(void) {
+  /* Loop forever */
+  for (;;)
+    ;
 }
